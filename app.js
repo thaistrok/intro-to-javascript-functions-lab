a9fdd4b7970ea3a3a88677eac73508b628083fb4
thaistrok <thaistrok@gmail.com>
2025-02-03 21:08:44 +0300

    home work

---
+++ app.js
@@ -0,0 +1,83 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+const isAdult = (age) => {
+    if (age >=18) {
+      return 'Adult';
+    } else {
+      return 'Minor';
+    }
+  }
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (character) => {
+    if (character === `a` || character === `e` || character === `i`) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+const generateEmail = (email,domain) => {
+  return `${email}@${domain}`
+}
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
